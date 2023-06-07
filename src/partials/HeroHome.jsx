0ba0966f1c11bc563565f96b22e0c00159fc0491
Rahul Talekar <38935787+Pix-ez@users.noreply.github.com>
2023-06-07 13:01:08 +0530

    Update HeroHome.jsx

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -55,7 +55,7 @@ function HeroHome() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">
-              Data Analysis Service
+              Data Visualization Service
             </h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
               This service provides easy features for users to vizualize data.
